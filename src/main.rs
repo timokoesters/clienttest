a5e91081ca7060922c81d451fea2b8ea75d57e9a
Timo Kösters <timo@koesters.xyz>
2023-07-06 16:31:47 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,143 @@
+use std::{env, process::exit};
+
+use matrix_sdk::{
+    config::SyncSettings,
+    room::Room,
+    ruma::events::room::{
+        member::StrippedRoomMemberEvent,
+        message::{MessageType, OriginalSyncRoomMessageEvent, RoomMessageEventContent},
+    },
+    Client,
+};
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // set up some simple stderr logging. You can configure it by changing the env
+    // var `RUST_LOG`
+    tracing_subscriber::fmt::init();
+
+    // our actual runner
+    login_and_sync().await?;
+    Ok(())
+}
+
+// The core sync loop we have running.
+async fn login_and_sync() -> anyhow::Result<()> {
+    // First, we set up the client.
+
+    // Note that when encryption is enabled, you should use a persistent store to be
+    // able to restore the session with a working encryption setup.
+    // See the `persist_session` example.
+    let client = Client::builder()
+        // We use the convenient client builder to set our custom homeserver URL on it.
+        .homeserver_url("https://conduit.koesters.xyz")
+        .build()
+        .await?;
+
+    // Then let's log that client in
+    client
+        .matrix_auth()
+        .login_username("@bot_timo:conduit.rs", "<password>")
+        .initial_device_display_name("getting started bot")
+        .await?;
+
+    // Now, we want our client to react to invites. Invites sent us stripped member
+    // state events so we want to react to them. We add the event handler before
+    // the sync, so this happens also for older messages. All rooms we've
+    // already entered won't have stripped states anymore and thus won't fire
+    client.add_event_handler(on_stripped_state_member);
+
+    // An initial sync to set up state and so our bot doesn't respond to old
+    // messages. If the `StateStore` finds saved state in the location given the
+    // initial sync will be skipped in favor of loading state from the store
+    let sync_token = client
+        .sync_once(SyncSettings::default())
+        .await
+        .unwrap()
+        .next_batch;
+
+    // now that we've synced, let's attach a handler for incoming room messages, so
+    // we can react on it
+    client.add_event_handler(on_room_message);
+
+    // since we called `sync_once` before we entered our sync loop we must pass
+    // that sync token to `sync`
+    let settings = SyncSettings::default().token(sync_token);
+    // this keeps state from the server streaming in to the bot via the
+    // EventHandler trait
+    client.sync(settings).await?; // this essentially loops until we kill the bot
+
+    Ok(())
+}
+
+// Whenever we see a new stripped room member event, we've asked our client to
+// call this function. So what exactly are we doing then?
+async fn on_stripped_state_member(
+    room_member: StrippedRoomMemberEvent,
+    client: Client,
+    room: Room,
+) {
+    if room_member.state_key != client.user_id().unwrap() {
+        // the invite we've seen isn't for us, but for someone else. ignore
+        return;
+    }
+
+    // looks like the room is an invited room, let's attempt to join then
+    if let Room::Invited(room) = room {
+        // The event handlers are called before the next sync begins, but
+        // methods that change the state of a room (joining, leaving a room)
+        // wait for the sync to return the new room state so we need to spawn
+        // a new task for them.
+        tokio::spawn(async move {
+            println!("Autojoining room {}", room.room_id());
+            let mut delay = 2;
+
+            while let Err(err) = room.accept_invitation().await {
+                // retry autojoin due to synapse sending invites, before the
+                // invited user can join for more information see
+                // https://github.com/matrix-org/synapse/issues/4345
+                eprintln!(
+                    "Failed to join room {} ({err:?}), retrying in {delay}s",
+                    room.room_id()
+                );
+
+                sleep(Duration::from_secs(delay)).await;
+                delay *= 2;
+
+                if delay > 3600 {
+                    eprintln!("Can't join room {} ({err:?})", room.room_id());
+                    break;
+                }
+            }
+            println!("Successfully joined room {}", room.room_id());
+        });
+    }
+}
+
+// This fn is called whenever we see a new room message event. You notice that
+// the difference between this and the other function that we've given to the
+// handler lies only in their input parameters. However, that is enough for the
+// rust-sdk to figure out which one to call one and only do so, when
+// the parameters are available.
+async fn on_room_message(event: OriginalSyncRoomMessageEvent, room: Room) {
+    // First, we need to unpack the message: We only want messages from rooms we are
+    // still in and that are regular text messages - ignoring everything else.
+    let Room::Joined(room) = room else { return };
+    let MessageType::Text(text_content) = event.content.msgtype else { return };
+
+    // here comes the actual "logic": when the bot see's a `!party` in the message,
+    // it responds
+    if text_content.body.contains("!party") {
+        let content = RoomMessageEventContent::text_plain("🎉🎊🥳 let's PARTY!! 🥳🎊🎉");
+
+        println!("sending");
+
+        // send our message to the room we found the "!party" command in
+        // the last parameter is an optional transaction id which we don't
+        // care about.
+        room.send(content, None).await.unwrap();
+
+        println!("message sent");
+    }
+}
